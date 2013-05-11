c096081deffe0d8941f8bfda63a9dca6904ab3de
Scott Thomas <Scott3377@hotmail.com>
2013-05-10 20:24:13 -0500

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,14 +15,22 @@ class User < ActiveRecord::Base
 
   #before_save { |user| user.email = email.downcase }
   before_save { email.downcase! }
+  before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
   			uniqueness: { case_sensitive: false },
   			format: { with: VALID_EMAIL_REGEX}
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
+
+  private
+
+    def create_remember_token
+      # Create the token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 
 end
